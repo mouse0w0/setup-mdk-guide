2584216a3f358aad6ba388ebc10fe865dd6b02f2
Mouse <mouse0w0@qq.com>
2021-10-01 16:06:54 +0800

    rename

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const nav = require('./nav.js');
 const sidebar = require('./sidebar.js');
 
 module.exports = {
-    title: '设置MDK指南',
+    title: '配置MDK指南',
     base: '/setup-mdk-guide/',
     markdown: {
         lineNumbers: true
